0d29995716b3039d38e21724e4012375ff3ec2cc
NARRA LEKHAN KUMAR <lekhannarra2003@gmail.com>
2023-05-19 18:55:41 +0530

    login-Signup

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,38 +1,63 @@
 import React from 'react';
 import '../styles/logins.css';
 import { Link } from 'react-router-dom';
+import Box from '@mui/material/Box';
+import { TextField } from '@mui/material';
 
 function Login() {
     return (
-        <div className='login templete d-flex justify-content-center align-items-center w-100 h-100 vh-100 bg-white'>
-            <div className='form_container p-5 rounded bg-white'>
-                <form>
-                    <h3 className='text-center'>Sign In</h3>
-                    <div className='mb-2'>
-                        <label htmlFor='Email'>Email</label>
-                        <input type='email' placeholder='enter email' className='form-control' />
-                    </div>
-                    <div className='mb-2'>
-                        <label htmlFor='password'>Password</label>
-                        <input type='password' placeholder='enter password' className='form-control' />
-                    </div>
-                    <div className='mb-2'>
-                        <input type='checkbox' className='custom-control custom-check' id='check' />
-                        <label htmlFor='check' className='custom-input-label ms-2'>
-                            Remember Me
-                        </label>
-                    </div>
-                    <div className='d-grid'>
-                        <button className='btn btn-primary'>Sign In</button>
-                    </div>
-                    <div>
-                        <p className='text-end mt-2'>
-                            <Link to="/signup" className='ms-2'>Sign up</Link>
-                        </p>
-                    </div>
-                </form>
+        <Box
+            component="form"
+            sx={{
+                '& .MuiTextField-root': { m: 1, width: '36ch' },
+            }}
+            noValidate
+            autoComplete="off"
+        >
+            <div className='login templete d-flex justify-content-center align-items-center vh-100 bg-white'>
+                <div className='form_container p-5 rounded bg-white'>
+                    <form>
+                        <h3 className='text-center'>Sign In</h3>
+                        <div className='mb-2'>
+                            <TextField
+                                id="outlined-email-input"
+                                label="Email"
+                                type="email"
+                                autoComplete="current-email"
+                            />
+                            {/* <TextField id="outlined-basic" label="email" variant="emai" /> */}
+                            {/* <label htmlFor='Email'>Email</label>    
+                            <input type='email' placeholder='enter email' className='form-control' /> */}
+
+                        </div>
+                        <div className='mb-2'>
+                            <TextField
+                                id="outlined-password-input"
+                                label="Password"
+                                type="password"
+                                autoComplete="current-password"
+                            />
+                            {/* <label htmlFor='password'>Password</label>
+                        <input type='password' placeholder='enter password' className='form-control' /> */}
+                        </div>
+                        <div className='mb-2'>
+                            <input type='checkbox' className='custom-control custom-check' id='check' />
+                            <label htmlFor='check' className='custom-input-label ms-2'>
+                                Remember Me
+                            </label>
+                        </div>
+                        <div className='d-grid'>
+                            <button className='btn btn-primary'>Sign In</button>
+                        </div>
+                        <div>
+                            <p className='text-end mt-2'>
+                                <Link to="/signup" className='ms-2'>Sign up</Link>
+                            </p>
+                        </div>
+                    </form>
+                </div>
             </div>
-        </div>
+        </Box>
     )
 }
 
--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,39 +1,68 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField'
 
 function Signup() {
-  return (
-    <div className='signup templete d-flex justify-content-center align-items-center vh-100 bg-white'>
-            <div className='form_container p-5 rounded bg-white'>
-                <form>
-                    <h3 className='text-center'>Sign Up</h3>
-                    <div className='mb-2'>
-                        <label htmlFor='fname'>First Name</label>
-                        <input type='text' placeholder='enter First Name' className='form-control' />
-                    </div>
-                    <div className='mb-2'>
-                        <label htmlFor='lname'>Last Name</label>
-                        <input type='email' placeholder='enter Last Name' className='form-control' />
-                    </div>
-                    <div className='mb-2'>
-                        <label htmlFor='Email'>Email</label>
-                        <input type='email' placeholder='enter email' className='form-control' />
-                    </div>
-                    <div className='mb-2'>
-                        <label htmlFor='password'>Password</label>
-                        <input type='password' placeholder='enter password' className='form-control' />
-                    </div>
-                    
-                    <div className='d-grid'>
-                        <button className='btn btn-primary'>Sign Up</button>
-                        <p className='text-end mt-2'>
-                            Already Registered <Link to="/" className='ms-2'>Sign in</Link>
-                        </p>
-                    </div>
-                </form>
+    return (
+        <Box
+            component="form"
+            sx={{
+                '& .MuiTextField-root': { m: 1, width: '36ch' },
+            }}
+            noValidate
+            autoComplete="off"
+        >
+
+            <div className='signup templete d-flex justify-content-center align-items-center vh-100 bg-white'>
+                <div className='form_container p-5 rounded bg-white'>
+                    <form>
+                        <h3 className='text-center'>Sign Up</h3>
+                        <div className='mb-2'>
+                            {/* <legend>First Name</legend> */}
+                            <TextField id="outlined-basic" label="First Name" variant="outlined" />
+                            {/* <label htmlFor='fname'>First Name</label>
+                            <input type='text' placeholder='enter First Name' className='form-control' /> */}
+                        </div>
+                        <div className='mb-2'>
+                            {/* <legend>Last Name</legend> */}
+                            <TextField id="outlined-basic" label="Last Name" variant="outlined" />
+                            {/* <label htmlFor='lname'>Last Name</label>
+                            <input type='text' placeholder='enter Last Name' className='form-control' /> */}
+                        </div>
+                        <div className='mb-2'>
+                            {/* <TextField id="outlined-basic" type="emial" label="Email" variant="outlined" /> */}
+                            <TextField
+                                id="outlined-email-input"
+                                label="Email"
+                                type="email"
+                                autoComplete="current-email"
+                            />
+                            {/* <label htmlFor='Email'>Email</label>
+                            <input type='email' placeholder='enter email' className='form-control' /> */}
+                        </div>
+                        <div className='mb-2'>
+                            <TextField
+                                id="outlined-password-input"
+                                label="Password"
+                                type="password"
+                                autoComplete="current-password"
+                            />
+                            {/* <label htmlFor='password'>Password</label>
+                            <input type='password' placeholder='enter password' className='form-control' /> */}
+                        </div>
+
+                        <div className='d-grid'>
+                            <button className='btn btn-primary'>Sign Up</button>
+                            <p className='text-end mt-2'>
+                                Already Registered <Link to="/" className='ms-2'>Sign in</Link>
+                            </p>
+                        </div>
+                    </form>
+                </div>
             </div>
-        </div>
-  )
+        </Box>
+    )
 }
 
 export default Signup
